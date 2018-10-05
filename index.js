b89fa1398d545bc19485876469bffae810e72392
beneficial-bundle-8122 <satyasinha94@gmail.com>
2018-10-05 20:43:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var array3 = [];
+  for (let i = 0; i < musicians.length; i++) {
+    array3.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array3
+}
+
+function johnLennonFacts (facts) {
+  var new_facts = [];
+  var i = 0
+  while (i < facts.length) {
+    new_facts.push(facts[i])
+  }
+  return new_facts
+}
